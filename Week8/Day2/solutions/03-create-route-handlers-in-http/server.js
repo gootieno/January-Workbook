c176dea48b13db5a6ab52b9b4bac14799ab1bb21
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-03-14 11:17:25 -0700

    create route handlers

--- Week8/Day2/solutions/03-create-route-handlers-in-http/server.js
+++ Week8/Day2/solutions/03-create-route-handlers-in-http/server.js
@@ -1,4 +1,5 @@
-const http = require('http');
+const http = require("http");
+const { url } = require("inspector");
 
 let nextDogId = 1;
 
@@ -34,15 +35,55 @@ const server = http.createServer((req, res) => {
     // Do not edit above this line
 
     // define route handlers here
+    if (req.method === "GET" && req.url === "/") {
+      const responseBody = "Dog Club";
+
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      return res.end(responseBody);
+    }
+
+    if (req.method === "GET" && req.url === "/dogs") {
+      const responseBody = "Dog Index";
+
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+      return res.end(responseBody);
+    }
+
+    // const string = req.url.split("/");
+
+    if (req.method === "GET" && req.url.startsWith("/dogs")) {
+      console.log("req url ", req.url);
+
+      const urlParts = req.url.split("/");
+      console.log("url parts ", urlParts);
+
+      if (urlParts.length === 3) {
+        const dogId = urlParts[urlParts.length - 1];
+
+        if (dogId !== "new") {
+          const resBody = `Dog details for dogId: ${dogId}`;
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/plain");
+          return res.end(resBody);
+        } else {
+          const resBody = `Dog create form`;
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "text/plain");
+          return res.end(resBody);
+        }
+      }
+    }
 
     // Do not edit below this line
     // Return a 404 response when there is no matching route handler
     res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    return res.end('No matching route handler found for this endpoint');
+    res.setHeader("Content-Type", "text/plain");
+    return res.end("No matching route handler found for this endpoint");
   });
 });
 
 const port = 5000;
 
-server.listen(port, () => console.log('Server is listening on port', port));
+server.listen(port, () => console.log("Server is listening on port", port));
