3b409808eb96cf584d9e5b2b06a91c8be251e796
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-03-15 10:38:59 -0700

    promises solution

---
+++ Week8/Day3/solutions/03-fetch-from-server/assets/js/your-code.js
@@ -0,0 +1,21 @@
+export function getAllDogs() {
+    // Your code here
+}
+
+export function getDogNumberTwo() {
+    // Your code here
+}
+
+export function postNewDog() {
+    // Your code here
+}
+
+export function postNewDogV2(name, age) {
+     // Your code here
+}
+
+export function deleteDog(id) {
+      // Your code here
+}
+
+
