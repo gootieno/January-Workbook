47a778631863bb8f643b116d96e32479cd65a7f9
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-03-15 13:09:09 -0700

    fetch from server solution

--- Week8/Day3/solutions/03-fetch-from-server/assets/js/your-code.js
+++ Week8/Day3/solutions/03-fetch-from-server/assets/js/your-code.js
@@ -1,21 +1,42 @@
 export function getAllDogs() {
-    // Your code here
+  // Your code here
+  return fetch("/dogs");
 }
 
 export function getDogNumberTwo() {
-    // Your code here
+  // Your code here
+  return fetch("/dogs/2");
 }
 
+// const params4 = new URLSearchParams({ foo: "1", bar: "2" });
+
 export function postNewDog() {
-    // Your code here
+  // Your code here
+  const dogInfo = new URLSearchParams({ name: "Hachi Roku", age: "2" });
+
+  return fetch("/dogs", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: dogInfo,
+  });
 }
 
 export function postNewDogV2(name, age) {
-     // Your code here
+  // Your code here
+
+  return fetch("/dogs", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: new URLSearchParams({ name, age }),
+  });
 }
 
 export function deleteDog(id) {
-      // Your code here
+  // Your code here
+  return fetch(`/dogs/${id}/delete`, {
+    method: "POST",
+    headers: {
+      AUTH: "ckyut5wau0000jyv5bsrud90y",
+    },
+  });
 }
-
-
