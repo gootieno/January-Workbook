a2698ebbfc1f0a9a529c1d6870a95605db2a04d4
gootieno <gotieno@geoffreys-MBP.attlocal.net>
2023-03-22 13:44:31 -0700

    short practice solutions

--- Week9/Day3/solution/02-data-on-elements/data.js
+++ Week9/Day3/solution/02-data-on-elements/data.js
@@ -1,1 +1,25 @@
 // Your code here
+window.addEventListener("DOMContentLoaded", () => {
+  const addButton = document.getElementById("add");
+  //   const form = document.querySelector("form");
+  addButton.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const name = document.getElementById("name");
+    const type = document.getElementById("type");
+
+    console.log("name and type ", name, type);
+
+    const li = document.createElement("li");
+    li.innerText = name.value;
+    li.dataset.type = type.value;
+    // li.setAttribute("data-type", type.value);
+
+    console.log("list item ", li);
+
+    const shoppingListContainer = document.getElementById("shopping-list");
+    shoppingListContainer.appendChild(li);
+
+    name.value = "";
+  });
+});
